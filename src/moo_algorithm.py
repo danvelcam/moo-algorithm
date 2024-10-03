0fc61a25bb58ce587dec713d062f786d57fa2473
danvelcam <danvelcam@alum.us.es>
2024-10-03 11:29:58 +0200

    feat: euclidean distance matrix function

--- src/moo_algorithm.py
+++ src/moo_algorithm.py
@@ -7,16 +7,28 @@ class MooAlgorithm():
         self.ng_size = neighbourhood_size
         self.max = max
         self.lambda_population = self.generate_lambda_population()
+        self.euclidean_distance  = self.euclidean_distance_matrix()
         
-
+    #Cuando generamos una nueva solucion se ha de verificar que se encuentre en el espacio de busqueda
     def generate_lambda_population(self):
         vectors = []
-        for i in range(self.p + 1):
+        for i in range(self.p):
             x1 = (i / self.p) * self.max
             x2 = self.max - x1
             vectors.append([x1, x2])
         return np.array(vectors)
     
+    def euclidean_distance_matrix(self):
+        matrix = []
+        for i in range(self.p):
+            row = []
+            for j in range(self.p):
+                vector_i = self.lambda_population[i]
+                vector_j = self.lambda_population[j]
+                row.append(np.linalg.norm(vector_i - vector_j))
+            matrix.append(row)
+        return np.array(matrix)
+                      
 
 
 MooAlgorithm(10,10,2,1.0)
